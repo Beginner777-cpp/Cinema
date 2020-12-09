0e73136de36672087f6c3c61072ca7b38022bc40
Beginner777-cpp <61560747+Beginner777-cpp@users.noreply.github.com>
2020-12-09 19:11:39 +0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 const black_widow = document.querySelector('.black_widow');
 const video_widow = document.querySelector('.video_widow');
 const black_widow_poster = document.querySelector('.black_widow_poster');
-
-window.addEventListener('scroll',()=>{
-    // if($('.black_widow').offset().top<50){
-    //     console.log(123);
-    // }
-    console.log($('body').scrollTop());
+let temp = 0;
+window.addEventListener('scroll', () => {
+    if ($(document).scrollTop() >= $('.black_widow').offset().top - 200) {
+        let temp =
+            $('.black_widow_poster').hide(0);
+        console.log(1);
+        $('.video_widow').stop().slideDown(500);
+    }
 })
-setTimeout(() => {
-    
-}, 1000);
