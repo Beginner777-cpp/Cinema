815ea4914d9a8c9c9e9f43de043cbb1836bcb1b8
test <test>
2020-12-09 13:43:01 +0500

    test

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const black_widow = document.querySelector('.black_widow');
+const video_widow = document.querySelector('.video_widow');
+const black_widow_poster = document.querySelector('.black_widow_poster');
+
+window.addEventListener('scroll',()=>{
+    // if($('.black_widow').offset().top<50){
+    //     console.log(123);
+    // }
+    console.log($('body').scrollTop());
+})
+setTimeout(() => {
+    
+}, 1000);
